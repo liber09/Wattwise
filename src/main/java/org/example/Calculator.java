c4a071f02bf66a53e69632686452dbdf97def95b
liber09 <linda.bergsangel@hotmail.com>
2023-08-25 19:39:41 +0200

    function that calculates the average price for 24h

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -6,8 +6,12 @@ import java.util.Map;
 public class Calculator {
 
     public static void maxMinAverage(Map<String, Integer> userPriceList) {
-        System.out.println("The least expensive hour this day is between " + getMinPrice(userPriceList) + " cent per kWh");
-        System.out.println("The most expensive hour this day is between " + getMaxPrice(userPriceList) + " cent per kWh");
+        System.out.println("The least expensive hour this day is between " +
+                getMinPrice(userPriceList) + " cent per kWh");
+        System.out.println("The most expensive hour this day is between " +
+                getMaxPrice(userPriceList) + " cent per kWh");
+        System.out.println("The average price this day is " +
+                getAveragePrice(userPriceList) + " cent per kWh");
     }
 
     private static String getMinPrice(Map<String, Integer> userPriceList) {
@@ -22,5 +26,10 @@ public class Calculator {
                 Map.Entry.comparingByValue()).getKey();
         Integer maxValue = userPriceList.get(maxKey);
         return maxKey + ", " + maxValue;
+    }
+
+    private static String getAveragePrice(Map<String, Integer> userPriceList) {
+        return String.valueOf(userPriceList.values()
+                .stream().mapToDouble(integer -> integer).average().orElse(0.0));
     }
 }
