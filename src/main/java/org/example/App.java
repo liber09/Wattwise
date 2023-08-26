a44e313d5d42080b68afaec17e1459e532b96049
liber09 <linda.bergsangel@hotmail.com>
2023-08-26 05:42:35 +0200

    Can now calculte lowest price during a 4 hour interval

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,12 +8,13 @@ public class App {
     public static void main(String[] args) {
 
         String userMenuInput;
+        boolean userWantsToExit = false;
         Map<String, Integer> priceList = new HashMap<>();
 
         do{
             userMenuInput = Menu.printMenu();
 
-            switch (userMenuInput) {
+            switch (userMenuInput.toLowerCase()) {
                 case "1" -> {
                     priceList = PriceData.getDataFromUser();
                     System.out.println(priceList);
@@ -22,8 +23,10 @@ public class App {
                 case "3" -> Calculator.getSortedPriceList(priceList);
                 case "4" -> Calculator.bestChargingHours(4, priceList);
                 case "5" -> priceList = PriceData.getDataFromFile();
+                case "e" -> userWantsToExit = true;
+                default -> System.out.println("Felaktigt val, försök igen!");
             }
-        }while(!userMenuInput.equalsIgnoreCase("e"));
+        }while(!userWantsToExit);
 
     }
 }
